59f083e882e04db55f4b4b87667d93e8fd6f1ef5
Gustavo Oliveira <gustavo.oliveira53@fatec.sp.gov.br>
2018-03-26 21:44:44 -0300

    commit 1.0
    card create

--- index.php
+++ index.php
@@ -2,12 +2,28 @@
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" type="text/css" href="_css/cards.css" />
 <body>
 
-<div class="w3-container w3-green">
+<div class="col-md-12 w3-container w3-green">
   <h1>Clima Tempo</h1> 
   <p>Previsão do tempo</p> 
 </div>
+
+<div class="col-md-5 card">
+    <div class="col-md-7 w3-container w3-center" >
+    <p>card1</p> 
+	<form action="busca.php" method="post">
+<input class="btn input_margintop" type="search" name="busca" placeholder="Digite o nome de uma Cidade">
+<input class="btn btn_textcenter bg-white" type="submit" class="solid" value="Buscar">
+</form>
+
+  </div>
+</div>
+
+</body>
+</html> 
+
 
 <div class="w3-row-padding">
   <div class="w3-third">
